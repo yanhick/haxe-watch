242f7f312fe52e920e5d122277ea184cd90d5e9c
yannick <yannick.dominguez@gmail.com>
2015-01-17 03:44:42 +0100

    added doc

--- haxe-watch.sh
+++ haxe-watch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Watches for file change in current directory
 while true; do
     inotifywait -e create -e modify -e delete -e move . && \
+        #On each file change, invoke Haxe compiler with arguments passed to script
         haxe $@
 done
 
